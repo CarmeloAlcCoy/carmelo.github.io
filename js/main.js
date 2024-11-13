fbd41d1f01ae3bba62848735a53aec4ce3b73b5d
Carmelo <carmeloalcarazcoy@gmail.com>
2024-11-13 19:40:17 +0000

    style: :lipstick: Add prettier and style files

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
-document.getElementById('timeline-long').style.display = 'none';
+document.getElementById('timeline-long').style.display = 'none'
 
 $('#collapseTimeline').on('show.bs.collapse', function () {
-  var element;
-  var element2;
-  element = document.getElementById('timeline-long');
-  element.style.display = '';
-  element2 = document.getElementById('timeline-short');
-  element2.style.display = 'none';
-  //console.log('Juan');
+    var element
+    var element2
+    element = document.getElementById('timeline-long')
+    element.style.display = ''
+    element2 = document.getElementById('timeline-short')
+    element2.style.display = 'none'
+    //console.log('Juan');
 })
 
 $('#collapseTimeline').on('hide.bs.collapse', function () {
-  var element;
-  var element2;
-  element = document.getElementById('timeline-short');
-  element.style.display = '';
-  element2 = document.getElementById('timeline-long');
-  element2.style.display = 'none';
-  //console.log('Juann');
+    var element
+    var element2
+    element = document.getElementById('timeline-short')
+    element.style.display = ''
+    element2 = document.getElementById('timeline-long')
+    element2.style.display = 'none'
+    //console.log('Juann');
 })
 
 $('#collapseTimeline').collapse('hide')
