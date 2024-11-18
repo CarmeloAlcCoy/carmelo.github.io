f7979c0ba9a2d755a84d2acaa44e4caeb095a43c
Carmelo <carmeloalcarazcoy@gmail.com>
2024-11-18 23:27:44 +0000

    refactor: :recycle: Removed bootstrap and add flex css to navbar.

--- js/main.js
+++ js/main.js
@@ -1,23 +1,32 @@
 document.getElementById('timeline-long').style.display = 'none'
 
-$('#collapseTimeline').on('show.bs.collapse', function () {
-    var element
-    var element2
-    element = document.getElementById('timeline-long')
-    element.style.display = ''
-    element2 = document.getElementById('timeline-short')
-    element2.style.display = 'none'
-    //console.log('Juan');
-})
+function toogleNavbar() {
+    const elem = document.getElementById('navbarCollapse')
+    if (elem.classList.contains('show')) {
+        elem.classList.remove('show')
+    } else {
+        elem.classList.add('show')
+    }
+}
 
-$('#collapseTimeline').on('hide.bs.collapse', function () {
-    var element
-    var element2
-    element = document.getElementById('timeline-short')
-    element.style.display = ''
-    element2 = document.getElementById('timeline-long')
-    element2.style.display = 'none'
-    //console.log('Juann');
-})
+// $('#collapseTimeline').on('show.bs.collapse', function () {
+//     var element
+//     var element2
+//     element = document.getElementById('timeline-long')
+//     element.style.display = ''
+//     element2 = document.getElementById('timeline-short')
+//     element2.style.display = 'none'
+//     //console.log('Juan');
+// })
 
-$('#collapseTimeline').collapse('hide')
+// $('#collapseTimeline').on('hide.bs.collapse', function () {
+//     var element
+//     var element2
+//     element = document.getElementById('timeline-short')
+//     element.style.display = ''
+//     element2 = document.getElementById('timeline-long')
+//     element2.style.display = 'none'
+//     //console.log('Juann');
+// })
+
+// $('#collapseTimeline').collapse('hide')
